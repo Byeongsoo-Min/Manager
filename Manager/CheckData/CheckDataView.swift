1512c95cd46fa36490dbfa88078e4be9c5ebaa8a
MBSoo <qud9783@khu.ac.kr>
2024-10-14 19:35:00 +0900

    [feat] 탭바 2번째 메뉴 데이터 확인하기 뷰

--- Manager/CheckData/CheckDataView.swift
+++ Manager/CheckData/CheckDataView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct CheckDataView: View {
+    @State private var toggleCard: Bool = false
+    @State private var togglePamplete: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("매니절")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.black)
+                Spacer()
+            }.padding()
+            HStack(spacing: 16) {
+                Text("전체")
+                    .fontWeight(.bold)
+                    .onTapGesture {
+                        toggleCard = false
+                        togglePamplete = false
+                    }
+                    .foregroundColor(!toggleCard && !togglePamplete ? .black : .gray) // 둘다 false일때
+                Text("명함")
+                    .fontWeight(.bold)
+                    .onTapGesture {
+                        toggleCard.toggle()
+                        togglePamplete = false
+                    }
+                    .foregroundColor(toggleCard ? .black : .gray)
+                Text("팜플렛")
+                    .fontWeight(.bold)
+                    .onTapGesture {
+                        togglePamplete.toggle()
+                        toggleCard = false
+                    }
+                    .foregroundColor(togglePamplete ? .black : .gray)
+                Spacer()
+            }.padding()
+            ScrollView {
+                VStack {
+                    
+                }
+            }
+            Spacer()
+            
+        }
     }
 }
 
