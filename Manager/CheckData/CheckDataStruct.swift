1f323f598e0603481c775984a8fe705b9fec752f
MBSoo <qud9783@khu.ac.kr>
2024-10-14 21:10:10 +0900

    [extension] 데이터 모델 작성

---
+++ Manager/CheckData/CheckDataStruct.swift
@@ -0,0 +1,14 @@
+//
+//  CheckDataStruct.swift
+//  Manager
+//
+//  Created by MBSoo on 10/14/24.
+//
+
+import Foundation
+
+
+enum sortedTool: String, Codable, CaseIterable {
+    case newest = "최신순"
+    case preferJob = "직무순"
+}
