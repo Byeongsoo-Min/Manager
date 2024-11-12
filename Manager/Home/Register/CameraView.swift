3cd40c7cb61b4f64f4d41071e4feddac318922ac
MBSoo <qud9783@khu.ac.kr>
2024-11-13 02:00:36 +0900

    [feat] 홈에서 연결될 카메라 뷰 완성

---
+++ Manager/Home/Register/CameraView.swift
@@ -0,0 +1,92 @@
+//
+//  CameraView.swift
+//  Manager
+//
+//  Created by MBSoo on 11/12/24.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    @ObservedObject var viewModel = CameraViewModel(isReturn: false)
+    var body: some View {
+        ZStack {
+            viewModel.cameraPreview.ignoresSafeArea()
+            VStack {
+                HStack {
+                    // 셔터사운드 온오프
+                    Button(action: {viewModel.switchSilent()}) {
+                        Image(systemName: viewModel.isSilentModeOn ?
+                              "speaker.fill" : "speaker")
+                        .foregroundColor(viewModel.isSilentModeOn ? .yellow : .white)
+                    }
+                    .padding(.horizontal, 30)
+                    
+                    // 플래시 온오프
+                    Button(action: {viewModel.switchFlash()}) {
+                        Image(systemName: viewModel.isFlashOn ?
+                              "bolt.fill" : "bolt")
+                        .foregroundColor(viewModel.isFlashOn ? .yellow : .white)
+                    }
+                    .padding(.horizontal, 30)
+                }
+                .font(.system(size:25))
+                .padding()
+                
+                Spacer()
+                
+                HStack{
+                    Button(action: {}) {
+                        if let previewImage = viewModel.recentImage {
+                            Image(uiImage: previewImage)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 75, height: 75)
+                                .clipShape(RoundedRectangle(cornerRadius: 15))
+                                .aspectRatio(1, contentMode: .fit)
+                        } else {
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(lineWidth: 3)
+                                .foregroundColor(.white)
+                                .frame(width: 75, height: 75)
+                        }
+                    }
+                    .padding()
+                    
+                    Spacer()
+                    
+                    // 사진찍기 버튼
+                    Button(action: {
+                        viewModel.capturePhoto()}) {
+                        Circle()
+                            .stroke(lineWidth: 5)
+                            .frame(width: 75, height: 75)
+                            .padding()
+                    }
+                    
+                    Spacer()
+                    
+                    // 전후면 카메라 교체
+                    Button(action: {viewModel.changeCamera()}) {
+                        Image(systemName: "arrow.triangle.2.circlepath.camera")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                        
+                    }
+                    .frame(width: 75, height: 75)
+                    .padding()
+                }
+            }
+            .foregroundColor(.white)
+        }
+        .opacity(viewModel.shutterEffect ? 0 : 1)
+        .onAppear(
+            perform: self.viewModel.configure
+        )
+    }
+}
+
+#Preview {
+    CameraView()
+}
