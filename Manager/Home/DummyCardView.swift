99719fd2852586eca01e401e7e3e2eab90bf5eaf
MBSoo <qud9783@khu.ac.kr>
2024-12-05 23:54:49 +0900

    [feat] 저장된 카드가 없을 때 사용할 카드뷰

--- Manager/Home/DummyCardView.swift
+++ Manager/Home/DummyCardView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct DummyCardView: View {
+    @State var isShowing: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Rectangle()
+            .overlay {
+                ZStack{
+                    Image("exampleCard")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+                VStack(spacing: 0){
+                    HStack(spacing: 0){
+                        Spacer()
+                        Button{
+                            isShowing.toggle()
+                        } label: {
+                            Image(systemName: "photo.artframe")
+                                .foregroundColor(.white)
+                                .font(.system(size: 32))
+                        }
+                    }
+                    .padding(.top, 25)
+                    
+                    Spacer()
+                    
+                    HStack(spacing: 0){
+                        Text("경희대학교") // 명함 회사
+                            .font(.system(size: 24))
+                            .bold()
+                            .foregroundColor(.white)
+                        Spacer()
+                    }
+                    .padding(.bottom, 8)
+                    HStack(spacing: 0){
+                        Text("010-1111-1111") // 명함 전화번호
+                            .font(.system(size: 17))
+                            .bold()
+                            .foregroundColor(.white)
+                        Spacer()
+                    }
+                    .padding(.bottom, 25)
+                    
+                }.padding(.horizontal, 20)
+            }
+            .foregroundColor(Color("cardBackgroundColor"))
     }
+        
 }
 
 #Preview {
