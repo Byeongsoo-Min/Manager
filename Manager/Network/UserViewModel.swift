04f987293057481656884c918b5dd5d61b221778
MBSoo <qud9783@khu.ac.kr>
2024-12-05 21:12:05 +0900

    [feat] 백엔드 회원가입 API 연결

--- Manager/Network/UserViewModel.swift
+++ Manager/Network/UserViewModel.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import Alamofire
+
+class UserViewModel: ObservableObject {
+    let baseUrl = APIClient.BASE_URL + "member/"
+    let session: Session = {
+        let interceptor = BaseInterceptor()
+        let configuration = URLSessionConfiguration.default
+        return Session(configuration: configuration, interceptor: interceptor)
+    }()
+    var managerName: String?
+    var memberId: Int?
+    
+    func signUp(manager_name: String){
+        let urlString = baseUrl + "signup/"
+        guard let url = URL(string: urlString) else {
+            print("error has occured in converting URL")
+            return
+        }
+        let timestamp = Int(Date().timeIntervalSince1970 * 1000)
+        let parameters: [String: Any] = [
+            "manager_name": manager_name
+        ]
+        session.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default)
+            .responseDecodable(of: AuthResponse.self, completionHandler: { response in
+                switch response.result {
+                case .success(let response):
+                    print(response)
+                    self.managerName = response.manager_name
+                    self.memberId = response.member_id
+                    UserDefaultsManager.shared.setConstants(managerName: manager_name, memberId: self.memberId ?? 11)
+                case .failure(let error):
+                    print(error.responseCode)
+                    print(error)
+                }
+            })
+    }
+    
+}
