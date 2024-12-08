a561b9431808463d466a2d316ff76809811a8d4f
MBSoo <qud9783@khu.ac.kr>
2024-12-08 21:49:34 +0900

    [fix] 역할에 맞는 디렉토리로 리빌딩

---
+++ Manager/ViewModel/CheckDataViewModel.swift
@@ -0,0 +1,76 @@
+//
+//  CheckDataViewModel.swift
+//  Manager
+//
+//  Created by MBSoo on 12/1/24.
+//
+
+import Foundation
+import Alamofire
+import UIKit
+
+struct Card: Codable {
+    var companyName: String
+    var companyNumber: String
+    var imageBase64: String
+    var cardId: Int
+}
+
+class CheckDataViewModel: ObservableObject {
+    @Published var cardList: [Card]?
+    @Published var nameSort = SortBy.nameASC
+    @Published var ratingSort = SortBy.ratingASC
+    var BASE_URL = APIClient.BASE_URL
+    
+    func alamofireNetworking() {
+        //MARK: URL생성, guard let으로 옵셔널 검사
+        guard let sessionUrl = URL(string: BASE_URL + "card/retrieve") else {
+            print("Invalid URL")
+            return
+        }
+        let parameters: Parameters = [
+            "memberId" : 11
+        ]
+        //MARK: Request생성
+        AF.request(sessionUrl,
+                   method: .get, // HTTP메서드 설정
+                   parameters: parameters, // 파라미터 설정
+                   encoding: URLEncoding.default) // 헤더 설정
+        //MARK: responseDecodable을 통해 UserDatas form으로 디코딩, response의 성공 여부에 따라 작업 분기
+        .responseDecodable(of: [Card].self) { response in
+            switch response.result {
+            case .success(let cards):
+                print("received Cards", cards)
+                cards.forEach { card in
+                    self.cardList?.append(card)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func alamofireFixtureDetail<T: Decodable>(url: String, itemId: Int) async throws -> T{
+        let sessionUrl = URL(string: url.appending("/\(itemId)"))
+        print("get fixture detail", sessionUrl)
+        //MARK: Request생성
+        
+        return try await AF.request(sessionUrl!,
+                   method: .get, // HTTP메서드 설정
+                   parameters: nil, // 파라미터 설정
+                   encoding: URLEncoding.default, // 인코딩 타입 설정
+                   headers: ["Content-Type":"application/json", "Accept":"application/json"]) // 헤더 설정
+//        .validate(statusCode: 200..<300) // 유효성 검사
+//        MARK: responseDecodable을 통해 UserDatas form으로 디코딩, response의 성공 여부에 따라 작업 분기
+        .serializingDecodable()
+        .value
+    }
+    
+    // For convinience. Sort type.
+    enum SortBy: String {
+        case nameASC = "Name △"
+        case nameDESC = "Name ▽"
+        case ratingASC = "Date △"
+        case ratingDESC = "Date ▽"
+    }
+}
---
+++ Manager/ViewModel/ConverstaionViewModel.swift
@@ -0,0 +1,72 @@
+//
+//  ConverstaionViewModel.swift
+//  Manager
+//
+//  Created by MBSoo on 12/6/24.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+import Alamofire
+
+
+class ConverstaionViewModel: ObservableObject {
+    @Published var contentList: [ChatResponse]?
+    @Published var chatList: [String: String]?
+    
+    var chatIdx = 0
+    var BASE_URL = APIClient.BASE_URL
+    var BASE_PARAMETERS = ChatRequestModel(model: "gpt-3.5-turbo", messages: [], member_id: UserDefaultsManager.shared.getMemeberId())
+    
+    init() {
+        self.chatList = UserDefaultsManager.shared.getAllChats()
+        self.chatIdx = (chatList?.count ?? 0)/2
+    }
+    
+    func setMessage(message: String) {
+        var messages: [Message] = []
+        messages.append(Message(role: "system", content: "Store each business card that is provided along with its tags. For every subsequent query:- Analyze the query for relevant keywords or details. - Retrieve the most appropriate business card based on the matches between the query and stored information. - If there are multiple suitable cards, return the one that most closely matches. The user's response is divided into /. Answer in korean."))
+        messages.append(Message(role: "user", content: message))
+        self.BASE_PARAMETERS.messages = messages
+        self.chatList?["user\(self.chatIdx)"] = message
+    }
+    
+    func sendUserchatToServer(message: String) {
+        //MARK: URL생성, guard let으로 옵셔널 검사
+        guard let sessionUrl = URL(string: BASE_URL + "chatGpt/prompt") else {
+            print("Invalid URL")
+            return
+        }
+        self.setMessage(message: message)
+        let parameters: Parameters = [
+            "model": self.BASE_PARAMETERS.model,
+            "message": self.BASE_PARAMETERS.messages,
+            "memberId" : self.BASE_PARAMETERS.member_id
+            
+        ]
+        let headers: HTTPHeaders = [
+            HTTPHeader.contentType("application/json")
+        ]
+        //MARK: Request생성
+        AF.request(sessionUrl,
+                   method: .post, // HTTP메서드 설정
+                   parameters: parameters, // 파라미터 설정
+                   encoding: URLEncoding.default,
+                   headers: headers) // 헤더 설정
+        //MARK: responseDecodable을 통해 UserDatas form으로 디코딩, response의 성공 여부에 따라 작업 분기
+        .responseDecodable(of: [ChatResponse].self) { response in
+            switch response.result {
+            case .success(let chats):
+                print("received Chats", chats)
+                chats.forEach { chat in
+                    self.chatList?["gpt\(self.chatIdx)"] = chat.content
+                    UserDefaultsManager.shared.saveUserChat(message: self.chatList?["user\(self.chatIdx)"] ?? "")
+                    UserDefaultsManager.shared.saveGptChats(message: chat.content)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
