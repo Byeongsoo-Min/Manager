2d2b7df290d641d07ff760c00ffded996e2d3344
MBSoo <qud9783@khu.ac.kr>
2024-12-10 20:29:25 +0900

    [feat] 카드를 확인할 수 있는 뷰 만듦

--- Manager/CheckData/ExistDataView.swift
+++ Manager/CheckData/ExistDataView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ExistDataView: View {
     @ObservedObject var observable: HomeObservable
-    
+    //MARK: pageNumber랑 저장된 컴패니의 갯수를 맞추는 방법을 찾아야함.
+    //MARK: 방법1: 키랑 이미지 같이 뱉는 펑션 만들고, substring으로 index 추출해서 hashtag 불러오기
+    //MARK: 방법2: remote 서버 테이블 다 드랍시키고 다시 만들기 
     var body: some View {
         Text("Hello \(observable.pageNumber ?? 10)")
     }
