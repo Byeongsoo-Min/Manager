937b4bd3a840d531a1060f045ab3504351f423bd
MBSoo <qud9783@khu.ac.kr>
2024-12-12 21:12:40 +0900

    [feat]Checkdata에서 보여지는 list Cell

--- Manager/Component/dataListCellView.swift
+++ Manager/Component/dataListCellView.swift
@@ -9,10 +9,9 @@ import SwiftUI
 
 struct dataListCellView: View {
     var isCard = true
-    var companyName = "경희대학교" //더미데이터 수정 필요
-    var hashtagArray = ["경기도", "용인", "컴퓨터공학과"] //더미데이터 수정 필요
     var addedDate = Date()
-    var card: Card = Card(companyName: "경희대학교", companyNumber: "010-2222-1234", image: "", cardId: 12)
+    let dateFormatter = DateFormatter()
+    let info: UserDefaultsManager.CompanyInfo
     var body: some View {
         VStack {
             HStack {
@@ -28,27 +27,28 @@ struct dataListCellView: View {
                         .foregroundColor(Color("playgroundColor"))
                 }
                 Spacer()
-                Text("\(addedDate.formatted())") // 추가된 날짜 백엔드에서 받아와서 오늘 날짜랑 계산해서 표기하기 (하루전 2일전..)
+                Text("\(calcDate(addedDate:addedDate))") // 추가된 날짜 백엔드에서 받아와서 오늘 날짜랑 계산해서 표기하기 (하루전 2일전..)
             }.padding()
             HStack {
-                Text(card.companyName)
+                Text(info.companyNameNum[0])
                 Spacer()
             } .padding(.horizontal)
             HStack {
-                ForEach(hashtagArray, id: \.self){ data in
+                ForEach(info.companyHashTag, id: \.self){ data in
                     Text("#\(data)")
                 }
                 Spacer()
             }.padding(.horizontal)
         }.padding()
     }
-    func calcDate(addedDate: Date){ // 백엔드에서 받아오는 날짜로 대신
-        var howFarDate = 0
-        print(Date().hashValue)
-        print(1234)
+    func calcDate(addedDate: Date) -> String{ // 백엔드에서 받아오는 날짜로 대신
+        dateFormatter.dateFormat = "yyyy년 MM월 dd일"
+        let formattedDate = dateFormatter.string(from: addedDate)
+        
+        return formattedDate
     }
 }
 
-#Preview {
-    dataListCellView()
-}
+//#Preview {
+//    dataListCellView()
+//}
