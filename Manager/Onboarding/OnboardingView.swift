eefbe3006959e2b2d259646c883c05931408619c
MBSoo <qud9783@khu.ac.kr>
2024-12-14 17:58:07 +0900

    [feat] 이미지에 맞는 텍스트가 표시되도록 변경

--- Manager/Onboarding/OnboardingView.swift
+++ Manager/Onboarding/OnboardingView.swift
@@ -11,7 +11,7 @@ struct OnboardingView: View {
     @State private var currentIndex = 0
     @State private var moveToNext = false
     let images = ["onboarding1", "onboarding2", "onboarding3"]
-    
+    let texts = ["간편한 스캔 기능", "손 쉬운 데이터 보기", "어디에서나 확인 가능"]
     var body: some View {
         NavigationStack{
             VStack(spacing: 24) {
@@ -26,20 +26,24 @@ struct OnboardingView: View {
                 GeometryReader { geometry in
                     TabView(selection: $currentIndex) {
                         ForEach(0..<images.count) { index in
-                            Image(images[index])
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: geometry.size.width, height: 300)
+                            VStack {
+                                Image(images[index])
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: geometry.size.width, height: 300)
+                                Text(texts[index])
+                                    .font(.title)
+                                    .fontWeight(.semibold)
+                            }
                                 .tag(index)
+                            
                         }
                     }
                     .tabViewStyle(PageTabViewStyle())
                     .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
                 }
                 
-                Text("간편한 스캔 기능")
-                    .font(.title)
-                    .fontWeight(.semibold)
+                
                 
                 Text("스마트폰으로 편하게 인식하고 \n 서비스를 이용하세요.")
                     .font(.body)
